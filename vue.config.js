ea19b416323a9eaa901bddbe212b16522f4d1f3b
Tung HOANG <tung.hoang@gmail.com>
2021-07-03 11:20:34 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,8 @@ const defaultSettings = require('./src/settings.js')
 // var https = require('https')
 // https.globalAgent.options.rejectUnauthorized = false
 const backend = {
-    //target: 'http://112.137.129.214:34280',
-    target: 'http://ipsman.duckdns.org:34280',
+    target: 'http://112.137.129.214:34280',
+    //target: 'http://ipsman.duckdns.org:34280',
     // target: 'http://10.10.1.155',
     secure: false,
     changeOrigin: true
